4c149ea7e3cc6caad7d5eac3503e616c3379aa1b
SomeToast56 <nathantwatt@gmail.com>
2026-08-28 17:26:39 -0700

    Tuning Part 1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/sensors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/sensors.java
@@ -1,21 +1,31 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class sensors {
     private TouchSensor touchSensor;
+    private double power;
+    private DcMotor testMotor;
     private boolean wasPressed = false;
     private int Counter = 0;
     public sensors(HardwareMap hardwareMap) {
         touchSensor = hardwareMap.get(TouchSensor.class, "touchsensor");
+        testMotor = hardwareMap.get(DcMotor.class, "testMotor");
     }
 
     public int TouchCounter() {
 
     // send the info back to driver station using telemetry function.
         if(touchSensor.isPressed() && !wasPressed) {
-            Counter++;
+            power = testMotor.getPower();
+            if(power > 0.5){
+                Counter++;
+            }
+            if(power < -0.5) {
+                Counter--;
+            }
         }
         wasPressed = touchSensor.isPressed();
     return Counter;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sensorTestNathan.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sensorTestNathan.java
@@ -4,21 +4,32 @@ import org.firstinspires.ftc.teamcode.mechanisms.sensors;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
 public class sensorTestNathan extends OpMode {
 
     private sensors sensors;
+    private DcMotor testMotor;
 
     @Override
     public void init(){
         sensors = new sensors(hardwareMap);
+        testMotor = hardwareMap.get(DcMotor.class, "testMotor");
     }
 
     @Override
     public void loop(){
         int numberOfPresses = sensors.TouchCounter();
-
+        if (gamepad1.aWasPressed()){
+            testMotor.setPower(1);
+        }
+        if (gamepad1.bWasPressed()){
+            testMotor.setPower(-1);
+        }
+        if (gamepad1.xWasPressed()){
+            testMotor.setPower(0);
+        }
         telemetry.addData("Touch Sensor Count", numberOfPresses);
         telemetry.update();
     }
