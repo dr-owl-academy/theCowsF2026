92f6abaf10d67d08eadb8ae50294e9f3f7beb605
SomeToast56 <nathantwatt@gmail.com>
2026-09-05 11:23:34 -0700

    Tuning Part 1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/sensors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/sensors.java
@@ -1,18 +1,22 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class sensors {
     private final TouchSensor touchSensor;
+    private final ColorSensor colorSensor;
     private double power;
-    private DcMotor testMotor;
+    private final DcMotor testMotor;
     private boolean wasPressed = false;
     private int Counter = 0;
     public sensors(HardwareMap hardwareMap) {
         touchSensor = hardwareMap.get(TouchSensor.class, "touchsensor");
         testMotor = hardwareMap.get(DcMotor.class, "testMotor");
+        colorSensor = hardwareMap.get(ColorSensor.class, "colorsensor");
     }
 
     public int TouchCounter() {
@@ -29,5 +33,12 @@ public class sensors {
         }
         wasPressed = touchSensor.isPressed();
     return Counter;
+    }
+    public int colorDetector(){
+        telemetry.addData("Red", colorSensor.red());
+        telemetry.addData("Green", colorSensor.green());
+        telemetry.addData("Blue", colorSensor.blue());
+        telemetry.update();
+        return 0;
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sensorTestNathan.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sensorTestNathan.java
@@ -1,25 +1,29 @@
 package org.firstinspires.ftc.teamcode;
+
 import org.firstinspires.ftc.teamcode.mechanisms.sensors;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
 @TeleOp
 public class sensorTestNathan extends OpMode {
 
     private sensors sensors;
     private DcMotor testMotor;
+    private ColorSensor colorSensor;
 
     @Override
     public void init(){
         sensors = new sensors(hardwareMap);
         testMotor = hardwareMap.get(DcMotor.class, "testMotor");
+        colorSensor = hardwareMap.get(ColorSensor.class,"colorsensor");
     }
 
     @Override
     public void loop(){
         int numberOfPresses = sensors.TouchCounter();
+
         if (gamepad1.aWasPressed()){
             testMotor.setPower(1);
         }
@@ -31,6 +35,14 @@ public class sensorTestNathan extends OpMode {
         }
         telemetry.addData("Touch Sensor Count", numberOfPresses);
         telemetry.update();
+
+        // Yellow = lots of red AND lots of green
+        if (colorSensor.green() > 0.5 && colorSensor.red() > 0.5) {
+            testMotor.setPower(1);
+        }
+        else {
+            testMotor.setPower(0);
+        }
     }
 }
 
