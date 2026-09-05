6adf85f23bb586b273f4aadb749a1500b583c653
fmafalcon9 <fma.falcon9@gmail.com>
2026-09-05 11:26:13 -0700

    I am a cow, I am a cow, I am a cow, I am a cow, I am a cow, I am a cow.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/sensors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/sensors.java
@@ -1,20 +1,29 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
 public class sensors {
 
     private TouchSensor touchSensor;
+    private ColorSensor colorSensor;
+
 
     private int touchCount = 0;
+    private int amountOfBlue = 0;
+    private int amountOfGreen = 0;
+    private int amountOfRed = 0;
     private boolean wasPressed = false;
 
     public sensors(HardwareMap hardwareMap) {
         touchSensor = hardwareMap.get(TouchSensor.class, "touchsensor");
+        colorSensor = hardwareMap.get(ColorSensor.class, "colorsensor");
     }
 
-    public void update() {
+
+    public int TouchCounter() {
+
         boolean isPressed = touchSensor.isPressed();
 
         if (isPressed && !wasPressed) {
@@ -22,9 +31,34 @@ public class sensors {
         }
 
         wasPressed = isPressed;
-    }
-
-    public int TouchCounter() {
         return touchCount;
     }
+    public int ColorSensorBlue(){
+        amountOfBlue = colorSensor.blue();
+
+        // returning all the variables from the color sensor
+        return amountOfBlue;
+
+
+
+    }
+    public int ColorSensorGreen(){
+        amountOfGreen = colorSensor.green();
+
+        // returning all the variables from the color sensor
+        return amountOfGreen;
+
+
+
+    }
+    public int ColorSensorRed(){
+        amountOfRed = colorSensor.red();
+
+        // returning all the variables from the color sensor
+        return amountOfRed;
+
+
+
+    }
+
 }
