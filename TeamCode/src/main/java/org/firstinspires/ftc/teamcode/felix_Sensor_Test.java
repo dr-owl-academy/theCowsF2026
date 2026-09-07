7dff711b053815a349ac88573573758e0865cb00
fmafalcon9 <fma.falcon9@gmail.com>
2026-09-07 09:38:59 -0700

    I am a cow, I am a cow, I am a cow, I am a cow, I am a cow, I am a cow.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/felix_Sensor_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/felix_Sensor_Test.java
@@ -2,25 +2,43 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.mechanisms.sensors;
 
-@TeleOp(name="felix_Sensor_Test")
-public class felix_Sensor_Test extends OpMode {
+            @TeleOp(name="felix_Sensor_Test")
+            public class felix_Sensor_Test extends OpMode {
 
-    private sensors sensors;
+                private sensors sensors;
+                private DcMotor testMotor;
+                private ColorSensor colorSensor;
 
-    @Override
-    public void init() {
-        sensors =new sensors(hardwareMap);
 
+                @Override
+                public void init() {
+                    sensors = new sensors(hardwareMap);
+                    testMotor = hardwareMap.get(DcMotor.class, "testMotor");
+                    colorSensor = hardwareMap.get(ColorSensor.class, "colorsensor");
+                }
+
+                @Override
+                public void loop() {
+                    sensors.TouchCounter();
+
+
+
+                    if (gamepad1.b) {
+                        testMotor.setPower(1);
+                    } else if (gamepad1.a) {
+                        testMotor.setPower(-1);
+                     }
+                    else{
+                        testMotor.setPower(0);
+                    }
+                    telemetry.addData("Touch Count", sensors.returntouchcount());
+                    telemetry.update();
     }
-
-    @Override
-    public void loop() {
-
-    }
-
-
 }
+
