2237562ce75274e4358c44d11c4126774c09c1fe
fmafalcon9 <fma.falcon9@gmail.com>
2026-09-10 17:20:03 -0700

    I am a cow, I am a cow, I am a cow, I am a cow, I am a cow, I am a cow.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/sensors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/sensors.java
@@ -11,68 +11,100 @@ public class sensors {
     private ColorSensor colorsensor;
     private DcMotor testMotor;
 
-
     private int touchCount = 0;
     private boolean wasPressed = false;
+
     private int amountOfRed = 0;
     private int amountOfBlue = 0;
     private int amountOfGreen = 0;
+
+    // Remembers if Pollen was detected
     private boolean wasSeen = false;
 
+
     public sensors(HardwareMap hardwareMap) {
+
         touchsensor = hardwareMap.get(TouchSensor.class, "touchsensor");
         colorsensor = hardwareMap.get(ColorSensor.class, "colorsensor");
-        testMotor = hardwareMap.get(DcMotor.class,"testMotor");
+        testMotor = hardwareMap.get(DcMotor.class, "testMotor");
     }
 
 
     public void ballCounter() {
 
         boolean isPressed = touchsensor.isPressed();
-        boolean isSeen = colorsensor.red() < 500;
+
+        // Pollen color range
+        boolean isSeen =
+                colorsensor.green() > 9500 &&
+                        colorsensor.red() > 6000 &&
+                        colorsensor.blue() > 2000 &&
+                        colorsensor.green() < 14000 &&
+                        colorsensor.red() < 8500 &&
+                        colorsensor.blue() < 4500;
 
         double power = testMotor.getPower();
 
-        if (isPressed && !wasPressed) {
-            if (power == 1){
-                touchCount++;
-            }
-            if (power == -1){
-                touchCount --;
-            }
 
+        // Remember that Pollen was seen
+        if (isSeen) {
+            wasSeen = true;
         }
 
-        wasPressed = isPressed;
 
+        // Only count if:
+        // 1. Pollen was seen FIRST
+        // 2. The touch sensor is newly pressed
+        if (wasSeen && isPressed && !wasPressed) {
+
+            // Intake
+            if (power > 0.5) {
+                touchCount++;
+            }
+
+            // Outtake
+            else if (power < -0.5) {
+                touchCount--;
+            }
+
+            // Reset so the next Pollen can be counted
+            wasSeen = false;
+        }
+
+
+        // Remember the button state
+        wasPressed = isPressed;
     }
-    public int returntouchcount(){
+
+
+    public int returntouchcount() {
 
         return touchCount;
     }
-    public void colors(){
+
+
+    public void colors() {
+
         amountOfRed = colorsensor.red();
-        amountOfGreen = colorsensor.red();
+        amountOfGreen = colorsensor.green();
         amountOfBlue = colorsensor.blue();
-
-
-
     }
-    public int returnred(){
+
+
+    public int returnred() {
+
         return amountOfRed;
     }
-    public int returnblue(){
+
+
+    public int returnblue() {
+
         return amountOfBlue;
     }
-    public int returngreen(){
+
+
+    public int returngreen() {
+
         return amountOfGreen;
     }
-
-
-
-
-
-
-
-
 }
