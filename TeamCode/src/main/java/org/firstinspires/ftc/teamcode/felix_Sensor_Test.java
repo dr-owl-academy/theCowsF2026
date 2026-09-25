8edeaaabe34d9dac9da54784f0002f99b7596fed
fmafalcon9 <fma.falcon9@gmail.com>
2026-09-24 19:35:12 -0700

    I am a cow, I am a cow, I am a cow, I am a cow, I am a cow, I am a cow.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/felix_Sensor_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/felix_Sensor_Test.java
@@ -7,41 +7,52 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.mechanisms.sensors;
 
-            @TeleOp(name="felix_Sensor_Test")
-            public class felix_Sensor_Test extends OpMode {
+@TeleOp(name="felix_Sensor_Test")
+public class felix_Sensor_Test extends OpMode {
 
-                private sensors sensors;
-                private DcMotor testMotor;
-                private ColorSensor colorSensor;
+    private sensors sensors;
+    private DcMotor testMotor;
+    private ColorSensor colorSensor;
 
+    @Override
+    public void init() {
+        sensors = new sensors(hardwareMap);
+        testMotor = hardwareMap.get(DcMotor.class, "testMotor");
+        colorSensor = hardwareMap.get(ColorSensor.class, "colorsensor");
+    }
 
-                @Override
-                public void init() {
-                    sensors = new sensors(hardwareMap);
-                    testMotor = hardwareMap.get(DcMotor.class, "testMotor");
-                    colorSensor = hardwareMap.get(ColorSensor.class, "colorsensor");
-                }
+    @Override
+    public void loop() {
 
-                @Override
-                public void loop() {
-                    sensors.ballCounter();
-                    sensors.senseColor();
+        // Update sensor readings
+        sensors.ballCounter();
+        sensors.senseColor();
 
+        // Motor controls
+        if (gamepad1.b) {
+            testMotor.setPower(1);
+        }
+        else if (gamepad1.a) {
+            testMotor.setPower(-1);
+        }
+        else {
+            testMotor.setPower(0);
+        }
 
+        // Color sensor values
+        telemetry.addData("Red", sensors.returnred());
+        telemetry.addData("Green", sensors.returngreen());
+        telemetry.addData("Blue", sensors.returnblue());
 
-                    if (gamepad1.b) {
-                        testMotor.setPower(1);
-                    } else if (gamepad1.a) {
-                        testMotor.setPower(-1);
-                     }
-                    else{
-                        testMotor.setPower(0);
-                    }
-                    telemetry.addData("Touch Count", sensors.returntouchcount());
-                    telemetry.addData("Red:", sensors.returnred());
-                    telemetry.addData("Blue:", sensors.returnblue());
-                    telemetry.addData("Green:", sensors.returngreen());
-                    telemetry.update();
+        // Ball detection
+        telemetry.addData("Pollen", sensors.returnPollen());
+        telemetry.addData("Red Nectar", sensors.returRedNectar());
+        telemetry.addData("Blue Nectar", sensors.returBlueNectar());
+
+        // Other values
+        telemetry.addData("Touch Count", sensors.returntouchcount());
+        telemetry.addData("Motor Power", testMotor.getPower());
+
+        telemetry.update();
     }
 }
-
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/sensors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/sensors.java
@@ -17,14 +17,14 @@ public class sensors {
     private int amountOfRed = 0;
     private int amountOfBlue = 0;
     private int amountOfGreen = 0;
+
     private int pollenCount = 0;
     private int redNectarCount = 0;
     private int blueNectarCount = 0;
+
     private boolean pollenSensed = false;
     private boolean redNectarSensed = false;
     private boolean blueNectarSensed = false;
-    double power = testMotor.getPower();
-
 
 
     public sensors(HardwareMap hardwareMap) {
@@ -39,18 +39,8 @@ public class sensors {
 
         boolean isPressed = touchsensor.isPressed();
 
-        // Pollen color range
+        double power = testMotor.getPower();
 
-
-
-
-
-        // Remember that Pollen was seen
-
-
-        // Only count if:
-        // 1. Pollen was seen FIRST
-        // 2. The touch sensor is newly pressed
         if (isPressed && !wasPressed) {
 
             // Intake
@@ -62,11 +52,7 @@ public class sensors {
             else if (power < -0.5) {
                 touchCount--;
             }
-
-            // Reset so the next Pollen can be counted
-
         }
-
 
         // Remember the button state
         wasPressed = isPressed;
@@ -74,50 +60,77 @@ public class sensors {
 
 
     public int returntouchcount() {
-
         return touchCount;
     }
 
 
-
     public boolean senseColor() {
 
+        // Get current color sensor readings
         amountOfRed = colorsensor.red();
         amountOfGreen = colorsensor.green();
         amountOfBlue = colorsensor.blue();
-        boolean pollenSensed = amountOfBlue >0  && amountOfGreen >0  && amountOfRed >0  && amountOfBlue <2  && amountOfGreen <3  && amountOfRed <2 ;
-        boolean redNectarSensed = amountOfBlue >0  && amountOfGreen >0  && amountOfRed >0  && amountOfBlue <2  && amountOfGreen <3  && amountOfRed <2 ;
-        boolean blueNectarSensed = amountOfBlue >0  && amountOfGreen >0  && amountOfRed >0  && amountOfBlue <2  && amountOfGreen <3  && amountOfRed <2 ;
-       return pollenSensed;
 
 
+        // Pollen
+        pollenSensed = amountOfGreen > 9500
+                        && amountOfRed > 6000
+                        && amountOfBlue > 2000
+                        && amountOfGreen < 14000
+                        && amountOfRed < 8500
+                        && amountOfBlue < 4500;
+
+
+        // Red Nectar
+        redNectarSensed = amountOfBlue > 50
+                && amountOfGreen > 2000
+                && amountOfRed > 2800
+                && amountOfBlue < 1500
+                && amountOfGreen < 3200
+                && amountOfRed < 5600;
+
+
+        // Blue Nectar
+        blueNectarSensed = amountOfBlue > 4900
+                && amountOfGreen > 500
+                && amountOfRed > 100
+                && amountOfBlue < 6600
+                && amountOfGreen < 1900
+                && amountOfRed < 1300;
+
+
+        // Return whether pollen was detected
+        return pollenSensed;
     }
 
 
-
-
-
     public int returnblue() {
-
         return amountOfBlue;
     }
 
 
     public int returngreen() {
-
         return amountOfGreen;
     }
+
+
     public int returnred() {
-
-        return amountOfRed  ;
+        return amountOfRed;
     }
-    public boolean returRedNectar(){
-        return redNectarSensed ;
 
+
+    public boolean returnPollen() {
+        return pollenSensed;
     }
-    public boolean returBlueNectar(){
-        return blueNectarSensed ;
 
+
+    public boolean returRedNectar() {
+        return redNectarSensed;
+    }
+
+
+    public boolean returBlueNectar() {
+        return blueNectarSensed;
     }
 
 }
